f111cde554347757b6c7f3909c45f7c5d63b4c8c
Zubair-05 <mullamdz0501@gmail.com>
2023-06-13 20:06:18 +0530

    completed anagram challenge

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,20 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if(str1.length !== str2.length) return false;
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  const str1Arr = [], str2Arr = [];
+  for(let i=0; i<str1.length; i++) {
+    str1Arr.push(str1[i]);
+    str2Arr.push(str2[i]);
+  }
+  str1Arr.sort();
+  str2Arr.sort();
+  for(let i=0; i<str1Arr.length; i++) {
+    if(str1Arr[i] !== str2Arr[i]) return false;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
